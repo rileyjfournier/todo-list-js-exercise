63423900bc4d300b1f6f52a89cc33c79c6ec489e
Riley Fournier <rileyjfournier@gmail.com>
2021-02-12 20:07:13 +0000

    OOP complete!

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-/*
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
-*/
 
 function newTask(title, description) {
   const task = {
@@ -38,10 +14,6 @@ function newTask(title, description) {
   return task;
 };
 
-
-
-// DRIVER CODE BELOW
-
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😰");
 const tasks = [task1, task2];
@@ -52,8 +24,3 @@ task1.logState();
 
 console.log(task1);
 
-/*
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
-*/
